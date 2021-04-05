0a86f8fce1937b678ff19c222b14c664bc2bd716
wpp5 <wpp5@georgetown.edu>
2021-04-05 02:07:01 -0400

    Refactor and Test Product Lookup

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,6 +1,7 @@
-
-from app.shopping import format_usd
-from app.shopping import calculate_tax
+import os
+from datetime import datetime
+from pandas import read_csv
+from app.shopping import format_usd, calculate_tax, lookup_product
 
 def test_format_usd():
     assert format_usd(3.75838) == "$3.76"
@@ -9,4 +10,23 @@ def test_format_usd():
 
 def test_calculate_tax():
     assert calculate_tax(100) == "TAX: $8.75"
+
+mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
+mock_products_df = read_csv(mock_products_filepath)
+mock_products = mock_products_df.to_dict("records")
+
+def test_lookup_product():
+    valid_result = lookup_product("8",mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': 8,
+        'name':'Product 8',
+        'price': 10.0
+    }
+
+invalid_result = lookup_product("88888888",mock_products)
+assert invalid_result == None 
+
+
 
