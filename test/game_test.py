b35de686f667559512becce716ec027fdaae6fee
wpp5 <wpp5@georgetown.edu>
2021-04-05 01:05:41 -0400

    Testing game

--- test/game_test.py
+++ test/game_test.py
@@ -1,4 +1,15 @@
 
-# TODO: import some code
+from app.game import determine_winner
 
-# TODO: test the code
+def test_determination_of_the_winner():
+    assert determine_winner("rock","rock") == "tie"
+    assert determine_winner("rock","paper") == "paper"
+    assert determine_winner("rock","scissors") == "rock"
+
+    assert determine_winner("paper","paper") == "tie"
+    assert determine_winner("paper","rock") == "paper"
+    assert determine_winner("paper","scissors") == "scissors"
+
+    assert determine_winner("scissors","scissors") == "tie"
+    assert determine_winner("scissors","rock") == "rock"
+    assert determine_winner("scissors","paper") == "scissors"
--- test/shopping_test.py
+++ test/shopping_test.py
@@ -4,6 +4,8 @@ from app.shopping import format_usd
 def test_format_usd():
     assert format_usd(3.75838) == "$3.76"
     assert format_usd(1000000) == "$1,000,000.00"
+    assert format_usd(0) == "$0.00"
 
+def test_calculate_tax():
+    assert calculate_tax(100) == "$8.75"
 
-print("test")
